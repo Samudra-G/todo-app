107415e4f92070c1ca501656fa171128b0f8e8e0
Samudra <samudramukhar@gmail.com>
2025-06-09 16:04:38 +0530

    added middleware

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,12 +4,18 @@ import (
 	"fmt"
 	"log"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func Main(){
 	fmt.Println("API server is starting...")
 
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:  "*",
+		AllowHeaders: "Origin, Content-Type, Accept",
+		AllowMethods:  "GET, POST, PUT, DELETE, OPTIONS",
+	}))
 	RegisterRoutes(app)
 	log.Fatal(app.Listen(":4000"))
 }
